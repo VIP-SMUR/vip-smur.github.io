b2e75086c56138ca68e83c58514c1f649cffc7ff
Patrick Kastner <patrick.kastner@gmail.com>
2025-05-04 15:03:04 -0400

    Improved run files

--- run_serve.bat
+++
@@ -1,9 +0,0 @@
-echo. 
-echo "###############"
-echo "Serving"
-echo "###############"
-echo.
-
-
-REM mkdocs serve -q
-mkdocs serve -q -a 127.0.0.1:8080
--- serve-docker.bat
+++ serve-docker.bat
@@ -2,6 +2,7 @@ REM Stop all running Docker containers
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 
 REM Run container with env file and quiet output
+
 docker run --rm -it ^
   -p 8080:8000 ^
   -v %cd%:/docs ^
